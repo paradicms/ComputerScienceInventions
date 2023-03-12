46fac29f7b0ed54025b03a9333b3703d1474a15e
Minor Gordon <minorg@server.fake>
2023-03-12 19:51:10 -0400

    move app-configuration into the data

--- dev.sh
+++ dev.sh
@@ -9,9 +9,6 @@ PROJECTS_DIR_PATH=$PWD/..
 cd $PROJECTS_DIR_PATH/paradicms/lib/py/ssg
 
 poetry run $PROJECTS_DIR_PATH/markdown-ssg-action/action.py \
-  --app work-search \
-  --app-configuration $PROJECTS_DIR_PATH/ComputerScienceInventions/app-configuration.json \
   --dev \
   --markdown-directory-path $PROJECTS_DIR_PATH/ComputerScienceInventions \
   --pipeline-id ComputerScienceInventions
-
