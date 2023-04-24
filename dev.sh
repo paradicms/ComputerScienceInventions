82cc101b425acc81e177ddf567e5a5ddb8b17fa3
Minor Gordon <minorg@server.fake>
2023-04-24 16:28:51 -0400

    adapt dev.sh against paradicms

--- dev.sh
+++ dev.sh
@@ -5,10 +5,17 @@ set -e
 cd `dirname "$(readlink -f "$0")"`
 
 PROJECTS_DIR_PATH=$PWD/..
+DATA_DIR_PATH=$PROJECTS_DIR_PATH/paradicms/data/ComputerScienceInventions
 
 cd $PROJECTS_DIR_PATH/paradicms/lib/py/ssg
 
-poetry run $PROJECTS_DIR_PATH/markdown-ssg-action/action.py \
-  --dev \
+poetry run $PROJECTS_DIR_PATH/markdown-etl-action/action.py \
+  --data-directory-path $DATA_DIR_PATH \
   --markdown-directory-path $PROJECTS_DIR_PATH/ComputerScienceInventions \
+  --pipeline-id ComputerScienceInventions \
+  "$@"
+
+poetry run $PROJECTS_DIR_PATH/ssg-action/action.py \
+  --data-directory-path $DATA_DIR_PATH \
+  --dev \
   --pipeline-id ComputerScienceInventions
