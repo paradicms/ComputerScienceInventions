e562211398b624d3b5e9be876da6df5e503c31fc
Minor Gordon <minorg@server.fake>
2023-04-26 11:44:36 -0400

    normalizing nomenclature around cache_dir_path, loaded_*, etc.

--- dev.sh
+++ dev.sh
@@ -10,12 +10,12 @@ DATA_DIR_PATH=$PROJECTS_DIR_PATH/paradicms/data/ComputerScienceInventions
 cd $PROJECTS_DIR_PATH/paradicms/lib/py/ssg
 
 poetry run $PROJECTS_DIR_PATH/markdown-etl-action/action.py \
-  --data-directory-path $DATA_DIR_PATH \
+  --loaded-data-directory-path $DATA_DIR_PATH \
   --markdown-directory-path $PROJECTS_DIR_PATH/ComputerScienceInventions \
   --pipeline-id ComputerScienceInventions \
   "$@"
 
 poetry run $PROJECTS_DIR_PATH/ssg-action/action.py \
-  --data-directory-path $DATA_DIR_PATH \
+  --data-path $DATA_DIR_PATH \
   --dev \
   --pipeline-id ComputerScienceInventions
