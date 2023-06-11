896b0735358df751cf6534323908e039c15421ce
Minor Gordon <minorg@server.fake>
2023-06-11 15:26:17 -0400

    update dev.sh with new paths

--- dev.sh
+++ dev.sh
@@ -5,19 +5,21 @@ set -e
 cd `dirname "$(readlink -f "$0")"`
 
 PROJECTS_DIR_PATH=$PWD/..
-DATA_DIR_PATH=$PROJECTS_DIR_PATH/paradicms/data/ComputerScienceInventions
+PROJECT_DIR_PATH=$PROJECTS_DIR_PATH/ComputerScienceInventions
+CACHE_DIR_PATH=$PROJECT_DIR_PATH/.paradicms/cache
+LOADED_DATA_DIR_PATH=$PROJECT_DIR_PATH/.paradicms/data
 
 cd $PROJECTS_DIR_PATH/paradicms/lib/py/ssg
 
 poetry run $PROJECTS_DIR_PATH/directory-etl-action/action.py \
-  --cache-directory-path $DATA_DIR_PATH/cached \
-  --input-directory-path $PROJECTS_DIR_PATH/ComputerScienceInventions \
-  --loaded-data-directory-path $DATA_DIR_PATH/loaded \
+  --cache-directory-path $CACHE_DIR_PATH \
+  --input-directory-path $PROJECT_DIR_PATH \
+  --loaded-data-directory-path $LOADED_DATA_DIR_PATH \
   --pipeline-id ComputerScienceInventions \
   "$@"
 
 poetry run $PROJECTS_DIR_PATH/ssg-action/action.py \
-  --cache-directory-path $DATA_DIR_PATH/cached \
-  --data-path $DATA_DIR_PATH/loaded \
+  --cache-directory-path $CACHE_DIR_PATH \
+  --data-path $LOADED_DATA_DIR_PATH \
   --dev \
   --pipeline-id ComputerScienceInventions
